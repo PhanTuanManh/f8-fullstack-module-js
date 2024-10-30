82f300b23a45cee2e10776149f5b9463ea17cf3d
PhanTuanManh <phantuanmanh.0501@gmail.com>
2024-10-30 21:08:58 +0700

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "576px",
+
+      md: "768px",
+
+      lg: "992px",
+
+      xl: "1200px",
+
+      "2xl": "1400px",
+
+      "3xl": "1600px",
+
+      "4xl": "1800px",
+    },
     extend: {
       fontFamily: {
         roboto: "Roboto, sans-serif",
