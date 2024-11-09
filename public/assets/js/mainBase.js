fc318f94b3319d6b6d31f2616e3eb25a2d78d279
PhanTuanManh <phantuanmanh.0501@gmail.com>
2024-11-09 23:28:37 +0700

    Lưu thay đổi cục bộ trước khi pull

--- public/assets/js/mainBase.js
+++ public/assets/js/mainBase.js
@@ -9,6 +9,7 @@ import {
   getProductsByCategory,
   sortProducts,
   getAllCategories,
+  getRandomProducts,
 } from "./fetchAPI.js";
 
 // Function to load all components concurrently
@@ -304,4 +305,49 @@ document.addEventListener("DOMContentLoaded", () => {
   const next = document.querySelector(".next");
   prev.addEventListener("click", () => changeSlide(-1));
   next.addEventListener("click", () => changeSlide(1));
+});
+
+getRandomProducts().then((products) => {
+  const productList = document.querySelector(".hot-sale-product");
+
+  products.forEach((product) => {
+    const productCard = document.createElement("div");
+    productCard.classList.add(
+      "hot-sale-product-item",
+      "relative",
+      "cursor-pointer",
+      "group"
+    );
+    const productImage =
+      product.images && product.images.length > 0
+        ? product.images[0]
+        : "./assets/images/default-product.jpg";
+
+    const discount =
+      product.discount > 0
+        ? `<span class="discount absolute top-0 right-0 text-[#FF6962] font-semibold px-3.5 text-[12px]">
+        -${product.discount}%
+      </span>`
+        : "";
+
+    const hot = product.hot
+      ? `  <span class="tag absolute top-0 left-0 text-white bg-[#FF6962] font-semibold px-3.5 text-[14px]">
+        Hot
+      </span>">`
+      : "";
+    productCard.innerHTML = `
+      <img loading="lazy" src="${productImage}" alt="${product.title}" class="w-full object-cover hover:scale-105 mp-transition-5" />
+      <div class="item-text flex flex-col justify-center text-center mt-[20px] gap-2 mb-[30px]">
+        <h5 class = "truncate ">${product.title}</h5>
+        <span>$${product.price}</span>
+      </div>
+      ${hot}
+      ${discount}
+    `;
+
+    productCard.addEventListener("click", () => {
+      window.location.href = `detail.html?id=${product.id}`;
+    });
+    productList.appendChild(productCard);
+  });
 });
